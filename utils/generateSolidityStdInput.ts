e8050159415f189d9f8e8233c2244a3355d7a9d1
Juan David Suarez <juansuarezespitia@gmail.com>
2023-04-16 19:46:48 -0500

    added helper for generating solidity std input

---
+++ utils/generateSolidityStdInput.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+
+import hre from 'hardhat';
+
+const generateSolidityStdInputForContract = async (contractName: string) => {
+  const contractArtifact = await hre.artifacts.readArtifact(contractName);
+  const buildInfo = await hre.artifacts.getBuildInfo(
+    `${contractArtifact.sourceName}:${contractArtifact.contractName}`
+  );
+
+  const filename = `${contractName}.stdin.json`;
+
+  await fs.promises.writeFile(
+    filename,
+    JSON.stringify(buildInfo?.input, null, 4)
+  );
+
+  console.log(`Generated Solidity Std Input in file ${filename}`);
+};
+
+export { generateSolidityStdInputForContract };
