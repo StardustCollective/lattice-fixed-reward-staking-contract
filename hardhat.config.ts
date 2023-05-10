52d5e841bcf5aa3b90bdfeee01492d52b86ffa71
Juan David Suarez <juansuarezespitia@gmail.com>
2023-05-10 12:42:21 -0500

    updated network configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,8 @@
 import * as dotenv from 'dotenv';
 import { HardhatUserConfig } from 'hardhat/config';
-
 import '@nomicfoundation/hardhat-toolbox';
+import { NetworkUserConfig } from 'hardhat/types';
+
 import { initializeDayJs } from './exlib/dayjs';
 
 dotenv.config();
@@ -11,6 +12,16 @@ initializeDayJs();
 const accounts = process.env.LATTICE_EXCHANGE_ACCOUNT_PK
   ? [process.env.LATTICE_EXCHANGE_ACCOUNT_PK]
   : [];
+
+const getNetworkConfig = (alias: string, url: string | undefined) => {
+  const network: Record<string, NetworkUserConfig> = {};
+
+  if (url) {
+    network[alias] = { url, accounts };
+  }
+
+  return network;
+};
 
 const config: HardhatUserConfig = {
   solidity: '0.8.18',
@@ -22,22 +33,10 @@ const config: HardhatUserConfig = {
         count: 100
       }
     },
-    mainnet: {
-      url: process.env.QUICKNODE_ETHEREUM_MAINNET,
-      accounts
-    },
-    goerli: {
-      url: process.env.QUICKNODE_ETHEREUM_GOERLI,
-      accounts
-    },
-    polygon: {
-      url: process.env.QUICKNODE_POLYGON_MAINNET,
-      accounts
-    },
-    mumbai: {
-      url: process.env.QUICKNODE_POLYGON_MUMBAI,
-      accounts
-    }
+    ...getNetworkConfig('mainnet', process.env.QUICKNODE_ETHEREUM_MAINNET),
+    ...getNetworkConfig('goerli', process.env.QUICKNODE_ETHEREUM_GOERLI),
+    ...getNetworkConfig('polygon', process.env.QUICKNODE_POLYGON_MAINNET),
+    ...getNetworkConfig('mumbai', process.env.QUICKNODE_POLYGON_MUMBAI)
   }
 };
 
